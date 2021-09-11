6a5c86bc44197a6dfe6d94033e0bff6dff399e39
Ben <BenRenato@users.noreply.github.com>
2021-09-11 23:45:31 +0100

    Full functionality implemented.
    Added ability to change "jobtitle" and "companyname" from template cover letter for inputted names. Also able to specify font and font size.

--- main.py
+++ main.py
@@ -1,22 +1,67 @@
-import os
 import pathlib
-import docx
 from docx import Document
+from docx.shared import Pt
 
 
 def main():
     print("This program currently only works with .docx files. If you have a Word compatibility file \n"
-          "please save it as a .docx file instead.")
+          "please save it as a .docx file instead.\n")
 
     # Get the current working directory
     current_dir = str(pathlib.Path(__file__).parent.resolve())
 
+    # Append name to file path for the target file path
     docx_path_path = get_target_document_path(current_dir)
 
+    # Get job title and company name applying for
+    job_title, company_name = get_job_title_and_company_name()
+
+    # Open the document
     document = open_document(docx_path_path)
 
-    for p in document.paragraphs:
-        print(p.text)
+    # Ask user for font and sizing
+    try:
+        style = get_current_style(document)
+    except Exception:
+        # style is None is shut PyCharm up
+        style = None
+        exit("Style font or sizing is incorrect.")
+
+    # Replace all instances of "jobtitle" and "thecompany" with corresponding inputs
+    replace_text("jobtitle", job_title, document, style)
+    replace_text("thecompany", company_name, document, style)
+
+    # Save the document
+    document.save('Ben Gomez Cover Letter ' + company_name + '.docx')
+
+
+def get_job_title_and_company_name():
+    job_title = str(input("Job title: "))
+    company_name = str(input("Company name: "))
+
+    return job_title, company_name
+
+
+def get_current_style(document):
+    # Get font and font size from user
+    style = document.styles['Normal']
+    font = style.font
+    font.name = str(input("Please enter a font name to use: "))
+    font.size = Pt(int(input("What font size: ")))
+
+    return style
+
+
+def replace_text(search, replace, document, style):
+    # Find matching words
+    for paragraph in document.paragraphs:
+        if search in paragraph.text:
+            print("Found match for " + search)
+
+            # Replace words and apply style as specified.
+            new_para = paragraph.text.replace(search, replace)
+            paragraph.text = new_para
+            paragraph.style = style
 
 
 def open_document(path_to_file):
@@ -27,9 +72,11 @@ def open_document(path_to_file):
         # Putting this here after the exit so PyCharm shuts up
         return
 
+    print("Successfully opened document.")
+
     return document
 
-
+# Ask for file name and return correct file path
 def get_target_document_path(current_dir):
     docx_file_name = str(input("Please type the name of the Word (.docx) file you want to edit: "))
 
